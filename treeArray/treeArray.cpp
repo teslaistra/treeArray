126c4bd16712ff77d7a3030076a9cecdfbe4b8f1
teslaistra <danyefimoff@gmail.com>
2020-04-16 21:31:40 +0300

    init

---
+++ treeArray/treeArray.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+
+
+int main() {
+	vector<float> tree;
+	int root=0;
+	int lenght=0;
+
+	cout << "Enter a number of nodes\n";
+	cin >> lenght;
+	lenght++;
+	cout << "Enter a root of tree\n";
+	cin >> root; 
+
+	tree.resize(lenght);
+
+	int parent, son; 
+
+	for (size_t i = 0; i < lenght-2; i++)
+	{
+		cout << "enter parent node\n";
+		cin >> parent;
+		cout << "enter son node\n";
+		cin >> son; 
+
+		tree.at(son) = parent; 
+	}
+
+	cout << "input done\n";
+
+	for (size_t i = 1; i < lenght; i++)
+	{
+		cout << "node " << i << " has sons: ";
+		for (size_t j = 0; j < lenght; j++)
+		{
+			if (tree.at(j) == i) cout << j << " ";
+		}
+		cout << "\n";
+	}
+}
+
